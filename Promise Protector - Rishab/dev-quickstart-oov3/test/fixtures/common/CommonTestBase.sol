46a72760208a668b0ec48f73c017e219fd56b50d
Rishab <45955038+rishabchakraborty@users.noreply.github.com>
2023-06-24 22:26:15 -0400

    Delete Promise Protector - Rishab directory

--- Promise Protector - Rishab/dev-quickstart-oov3/test/fixtures/common/CommonTestBase.sol
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: AGPL-3.0-only
-pragma solidity ^0.8.0;
-
-import "forge-std/Test.sol";
-import "./TestAddress.sol";
-
-contract CommonTestBase is Test {}
--- Promise Protector - Rishab/dev-quickstart-oov3/test/fixtures/common/TimerFixture.sol
+++
@@ -1,9 +0,0 @@
-pragma solidity 0.8.16;
-
-import "@uma/core/contracts/common/implementation/Timer.sol";
-
-contract TimerFixture {
-    function setUp() public returns (Timer timer) {
-        return new Timer();
-    }
-}
